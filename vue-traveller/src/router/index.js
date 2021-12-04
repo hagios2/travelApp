68093129cca9b8e21071004c1a21a91ba6f300b0
Emmanuel Oteng Wilson <hagioswilson@gmail.com>
2021-12-04 22:04:37 +0000

    made changes

--- vue-traveller/src/router/index.js
+++ vue-traveller/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import sourceData from '@/data.json'
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,25 @@ const routes = [
         /* webpackChunkName: "destination details" */ "@/views/DestinationShow.vue"
       ),
     props: (route) => ({ ...route.params, id: parseInt(route.params.id) }),
+    beforeEnter: (to) => {
+      const exists = sourceData.destinations.find(
+        (destination) => parseInt(to.params.id) === destination.id);
+
+      if(!exists) {
+
+        return {
+
+          name: 'NotFound',
+
+          params: {pathMatch: to.path.split('/').slice(1)},
+
+          query: to.query,
+
+          hash: to.hash
+        
+        }
+      }
+    },
     children: [
       {
         path: ":experienceSlug",
@@ -41,6 +61,15 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior(to, from, savedPosition) {
+
+    return savedPosition || new Promise((resolve) => { 
+      
+      setTimeout(() => {
+        resolve({ top: 0, behavior: 'smooth'})
+      }, 300)
+    })
+  }
 });
 
 export default router;
